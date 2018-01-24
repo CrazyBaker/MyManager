824cc59f08f06e91a682c8ad7ad1cd3361dbfd8d
CrazyBaker <zigunovChrome@gmail.com>
2018-01-24 22:56:23 +1100

    0.5 Added saves for base skils

--- MyManager/Assets/Scripts/AddingSystem.cs
+++ MyManager/Assets/Scripts/AddingSystem.cs
@@ -19,9 +19,12 @@ private float progress;
 private float ost;
 private float all;
 
+
 	void Start () {
 		TextBox = FindObjectOfType<InputField>();
 		ost = baseCount;
+
+		SetupSaves();
 	}
 	
 	void Update () {
@@ -39,6 +42,7 @@ private float all;
 			LvlUp();
 		}
 		UpdateText();
+		SaveSkills();
 	}
 
 	public void LvlUp () {
@@ -52,5 +56,81 @@ private float all;
 		progressText.text = progress.ToString();
 		ostText.text = ost.ToString();
 		allText.text = all.ToString();
+	}
+
+	public void SetupSaves() {
+		if (PlayerPrefs.HasKey("plvl") & gameObject.name == "PButton") {
+			lvl = PlayerPrefs.GetFloat("plvl");
+			progress = PlayerPrefs.GetFloat("pprogress");
+			ost = PlayerPrefs.GetFloat("post");
+			all = PlayerPrefs.GetFloat("pall");
+		}
+
+		if (PlayerPrefs.HasKey("rlvl") & gameObject.name == "RButton") {
+			lvl = PlayerPrefs.GetFloat("rlvl");
+			progress = PlayerPrefs.GetFloat("rprogress");
+			ost = PlayerPrefs.GetFloat("rost");
+			all = PlayerPrefs.GetFloat("rall");
+		}
+
+		if (PlayerPrefs.HasKey("elvl") & gameObject.name == "EButton") {
+			lvl = PlayerPrefs.GetFloat("elvl");
+			progress = PlayerPrefs.GetFloat("eprogress");
+			ost = PlayerPrefs.GetFloat("eost");
+			all = PlayerPrefs.GetFloat("eall");
+		}
+
+		if (PlayerPrefs.HasKey("slvl") & gameObject.name == "SButton") {
+			lvl = PlayerPrefs.GetFloat("slvl");
+			progress = PlayerPrefs.GetFloat("sprogress");
+			ost = PlayerPrefs.GetFloat("sost");
+			all = PlayerPrefs.GetFloat("sall");
+		}
+
+		if (PlayerPrefs.HasKey("clvl") & gameObject.name == "CButton") {
+			lvl = PlayerPrefs.GetFloat("clvl");
+			progress = PlayerPrefs.GetFloat("cprogress");
+			ost = PlayerPrefs.GetFloat("cost");
+			all = PlayerPrefs.GetFloat("call");
+		}
+		
+		UpdateText ();
+	}
+
+	public void SaveSkills() {
+		if (gameObject.name == "PButton") {
+			PlayerPrefs.SetFloat("plvl", lvl);
+			PlayerPrefs.SetFloat("pprogress", progress);
+			PlayerPrefs.SetFloat("post", ost);
+			PlayerPrefs.SetFloat("pall", all);
+		}
+
+		if (gameObject.name == "RButton") {
+			PlayerPrefs.SetFloat("rlvl", lvl);
+			PlayerPrefs.SetFloat("rprogress", progress);
+			PlayerPrefs.SetFloat("rost", ost);
+			PlayerPrefs.SetFloat("rall", all);
+		}
+
+		if (gameObject.name == "EButton") {
+			PlayerPrefs.SetFloat("elvl", lvl);
+			PlayerPrefs.SetFloat("eprogress", progress);
+			PlayerPrefs.SetFloat("eost", ost);
+			PlayerPrefs.SetFloat("eall", all);
+		}
+		
+		if (gameObject.name == "SButton") {
+			PlayerPrefs.SetFloat("slvl", lvl);
+			PlayerPrefs.SetFloat("sprogress", progress);
+			PlayerPrefs.SetFloat("sost", ost);
+			PlayerPrefs.SetFloat("sall", all);
+		}
+
+		if (gameObject.name == "CButton") {
+			PlayerPrefs.SetFloat("clvl", lvl);
+			PlayerPrefs.SetFloat("cprogress", progress);
+			PlayerPrefs.SetFloat("cost", ost);
+			PlayerPrefs.SetFloat("call", all);
+		}
 	}
 }
