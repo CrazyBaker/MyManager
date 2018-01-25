191a095c58b6e982ffe423ecb11cbef7840256d3
CrazyBaker <zigunovChrome@gmail.com>
2018-01-25 22:30:56 +1100

    no message

---
+++ MyManager/Assets/SwichScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwichScreen : MonoBehaviour {
+
+	public Camera mainCamera;
+	//public Camera PCamera;
+	public Canvas canvas;
+	void Start () {
+		mainCamera.enabled = true;
+		//PCamera.enabled = false;
+		
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Q)) {
+			canvas.enabled = !canvas.enabled;
+		}
+	}
+
+	public void SwichOnP() {
+		mainCamera.enabled = !mainCamera.enabled;
+	//	PCamera.enabled = !PCamera.enabled;
+	} 
+}
---
+++ MyManager/Assets/time.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour {
+
+private Text text;
+private System.DateTime startTime;
+private string timeFromStart;
+
+	void Start () {
+		text = GetComponent<Text>();
+
+		startTime = System.DateTime.Now;
+	}
+	
+	void Update () {
+		System.DateTime nowTime;
+		nowTime = System.DateTime.Now;
+		timeFromStart = startTime.Subtract(nowTime).ToString();
+
+		text.text = timeFromStart;
+	}
+}
